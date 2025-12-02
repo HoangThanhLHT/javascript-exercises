0840ca5d86107ab68e424dcf4fad3280ec4cb814
HoangThanh <191544829+HoangThanhLHT@users.noreply.github.com>
2025-12-02 20:15:55 +0700

    done 1 - 11 exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let returnArray = args[0]
+    for (let i = 1; i < args.length; i++) {
+        let removeItem = args[i]
+            while (returnArray.includes(removeItem)) {
+                returnArray.splice(returnArray.indexOf(removeItem), 1)
+            }
+        }
+    return returnArray
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {return 'ERROR'}
+    let start = 0
+    let end = 0
+    if (num1 < num2) {
+        start = num1
+        end = num2
+    } else {
+        start = num2
+        end = num1
+    }
+    let sum = 0
+    for (let i = start; i <= end; i++) {
+        sum += i
+    }
+    return sum
 };
+
+function isIntegers(value) {
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
