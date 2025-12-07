5e4816122440d7799e5fd28c4d80d1da3bf24cda
HoangThanh <191544829+HoangThanhLHT@users.noreply.github.com>
2025-12-07 21:13:08 +0700

    done 12-16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (givenString) {
+    let cleanString = givenString.toLowerCase();
+    const removedLetter = [' ', '!', '.', ','];
+    for (let char of removedLetter) {
+        let text = cleanString.split(char).join('');
+        cleanString = text
+    };
+    return cleanString == cleanString.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    member = parseInt(member);
+    if (member == 0) {return 0};
+    if (member < 0) return 'OOPS';
+    let fibonacciNumber = [1, 1];
+    if (member < 3) {return 1}
+    for (let i = 3; i <= member; i ++) {
+        fibonacciNumber.push(fibonacciNumber.at(-1) + fibonacciNumber.at(-2));
+    }
+    return fibonacciNumber.at(-1);
 };
 
 // Do not edit below this line
