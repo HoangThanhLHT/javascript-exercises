048d3f0dd8bf51d44c3a8dec43fce019a85560bf
HoangThanh <191544829+HoangThanhLHT@users.noreply.github.com>
2025-12-07 21:12:36 +0700

    done 12-16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function getAge(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const personAge = getAge(person.yearOfDeath, person.yearOfBirth);
+        return (oldestAge > personAge) ? oldest : person;
+    });
 };
 
 // Do not edit below this line
